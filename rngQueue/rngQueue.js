01d2c598248f7b329606ad50fdf4cae44c0d4f3e
Dion Hobdy <devdionhobdy@gmail.com>
2021-09-07 17:11:26 -0700

    Update rngQueue.js

--- rngQueue/rngQueue.js
+++ rngQueue/rngQueue.js
@@ -1,5 +1,5 @@
-let obj = {}; // empty object. this will be our central object used for arr
-let arr = []; // empty array. this will contain duplicated objects
+let obj = {};
+let arr = [];
 
 function randomInt(max) {
     return Math.floor(Math.random() * max);
@@ -8,11 +8,11 @@ function randomInt(max) {
 let randomObjInt = randomInt(10); // randomInt should be used to generate a random number of duplicated objects. indexed 0 - 9
 
 for (let i = 0; i < randomObjInt; i++) {
-    let arrObj = Object.create(obj); // create a new object from obj
-    arrObj.num = 0; // num will be used to delete the object after it is returned when it doesn't equate to 0
+    let arrObj = Object.create(obj);
+    arrObj.num = 0;
     arrObj.id = i; // mark the id of the object based on i 
     arrObj.item = randomInt(100); // geneate a random value for the object's 'item'
-    arr.push(arrObj); // push the object into arr
+    arr.push(arrObj);
 } // for loop that creates duplicate objects. number of objects is based on randomObjInt
 
 arr.sort((a, b) => { return a.item - b.item }); // sort arr based on the objects 'item' integer value from lowest to highest
