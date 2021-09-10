5b0f898624889d4783f2859e0731418147030463
Dion Hobdy <devdionhobdy@gmail.com>
2021-09-10 01:22:30 -0700

    Update vanilla.js

--- vanilla/vanilla.js
+++ vanilla/vanilla.js
@@ -1,4 +1,4 @@
-const data = require('./vanilla.json');
+const data = require('./vanilla.json'); // import the json file of choice
 
 let arr = [];
 let obj = {};
@@ -6,21 +6,19 @@ let obj = {};
 for (let i = 0; i < 5; i++) {
     let arrObj = Object.create(obj);
     arrObj.id = i;
-    arrObj.item = data.item[i];
+    arrObj.item = data.item[i]; // set the object item to the value of i
     arrObj.num = 0;
     arr.push(arrObj);
-}
+} // create a set number of objects and push them into the empty array
 
 arr.sort((a, b) => {
     return a.item - b.item;
-});
+}); // sort the array from lowest item value to highest
 
 for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+    return arr[i];
     arr[i].num++;
     if (arr[i].num != 0) {
         delete arr[i];
     }
-}
-
-console.log(arr);
+} // distribute and then delete array objects
